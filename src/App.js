7516a9a81c60b9d165f52ab4d264986a298c781b
devrim safak ilhan <dvrmilhan@gmail.com>
2021-01-19 04:13:58 +0300

    PostPage added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import './theme.css';
 import PageContent from '../src/components/PageContent/PageContent';
 import Navbar from '../src/components/Navbar/Navbar'
+import Post from "../src/components/Post/Post"
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar></Navbar>
-			<PageContent></PageContent>
+			<Post />
 		</div>
 	);
 }
