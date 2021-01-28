dbd200ff478802164a2016831b2a92221decf883
Burak Hacıhan <burakmedinovski@gmail.com>
2021-01-29 01:16:52 +0300

    api baglantilari yapilmaya baslandi

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,24 +1,29 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 // const onClick = () => {
 // 	'.heart'.on('click', function () {
 // 		this.toggleClass('heart-blast');
 // 	});
 // };
 
-function Card() {
+function Card({items}) {
+
 	return (
 		<>
 			<div className="card card-pin">
-				<img
-					className="card-img"
-					src="https://images.unsplash.com/photo-1489743342057-3448cc7c3bb9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6d284a2efbca5f89528546307f7e7b87&auto=format&fit=crop&w=500&q=60"
-					alt="Card image"
-				/>
-				<div className="overlay">
+				<Link to={`/photo/${items.id}`}>
+					<img
+						className="card-img"
+						src={`${items.urls.small}`}
+						alt="Card image"
+					/>
+				</Link>
+				{/* <div className="overlay">
 					<div className="heart"></div>
 				</div>
-				<a href="post.html"></a>
+				<Link to={`/photo/${items.id}`} /> */}
 			</div>
 		</>
 	);
--- src/components/PageContent/PageContent.js
+++ src/components/PageContent/PageContent.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import {React,useEffect,useState} from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Card from '../Card/Card';
-import Filter from '../Filter/Filter';
 import Footer from '../Footer/Footer';
+import axios from 'axios';
+
+import PostDetail from '../Post/Post';
 
 function PageContent() {
+
+	const [photoCollection,setPhotoCollection] = useState([]);
+
+	useEffect(() => {
+
+		axios.get(`${process.env.REACT_APP_ENDPOINT_URL}collections/2489501/photos?client_id=${process.env.REACT_APP_Unsplash_Key}`).then((res) => {
+			setPhotoCollection(res.data);
+		})
+
+	},[])
+
 	return (
 		<>
-			<main role="main">
-				<section className="mt-4 mb-5">
-					<Filter></Filter>
-					<div className="container-fluid">
-						<div className="row">
-							<div className="card-columns">
-								<Card></Card>
+		<Router>
+			<Switch>
+				<Route path="/" exact>
+					<main role="main">
+						<section className="mt-4 mb-5">
+							<div className="container-fluid">
+								<div className="row">
+									<div className="card-columns">
+										{
+											photoCollection?.map((items,i) => (
+												<Card key={i} items={items}></Card>
+											))
+										}
+									</div>
+								</div>
 							</div>
-						</div>
-					</div>
-				</section>
-			</main>
+						</section>
+					</main>
+				</Route>
+				<Route path={`/photo/:id`} component={PostDetail} />
+			</Switch>
+		</Router>
 			<Footer></Footer>
 		</>
 	);
